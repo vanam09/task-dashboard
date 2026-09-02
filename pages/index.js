758bcddc691e953a38b15898fb14ba16ac735517
Sri Vanam <vanam09@hotmail.com>
2026-09-02 15:18:31 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>✅ Dashboard works!</h1>
+      <p>If you see this, Next.js is running correctly.</p>
+    </div>
+  );
+}
