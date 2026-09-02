beb463576aadc5cb7e6055b7f064556a6761ce87
Sri Vanam <vanam09@hotmail.com>
2026-09-02 14:51:08 -0400

    Delete pages/pages/api/next.config.js

--- pages/pages/api/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // This ensures API routes are not statically generated
-  output: 'standalone',
-};
-
-module.exports = nextConfig;
